79816a8d023239cbdfa1401ca47a93509cdbc5e7
brina-cloud <s.kaliza1@alustudent.com>
2025-10-15 07:57:48 -0700

    edges concat

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 
-print(f"{chr(111)}{chr(98)}{chr(106)}{chr(101)}{chr(99)}{chr(116)}-{chr(111)}{chr(114)}{chr(105)}{chr(101)}{chr(110)}{chr(116)}{chr(101)}{chr(100)} {chr(112)}{chr(114)}{chr(111)}{chr(103)}{chr(114)}{chr(97)}{chr(109)}{chr(109)}{chr(105)}{chr(110)}{chr(103)} {chr(119)}{chr(105)}{chr(116)}{chr(104)} {chr(80)}{chr(121)}{chr(116)}{chr(104)}{chr(111)}{chr(110)}")
+print(str[39:67] + str[107:112] + str[0:6])
