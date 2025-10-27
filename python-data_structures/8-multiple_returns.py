c8703f51f5b31e4fe1fac8194c8cbd9ea5701c5c
brina-cloud <s.kaliza1@alustudent.com>
2025-10-27 07:44:22 -0700

    8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+       while TRUE:
+           sentence.insert(0, None)
+           first = sentence[0]
+    else:
+        first = sentence[0]
+        length = len(sentence)
+    return (length, first)
