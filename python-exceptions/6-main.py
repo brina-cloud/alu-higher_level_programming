8afbebfb62f7ec0ffbd8244d6b3af05aa6c9023e
brina-cloud <s.kaliza1@alustudent.com>
2025-10-31 05:08:34 -0700

    6

--- python-exceptions/6-main.py
+++ python-exceptions/6-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-raise_exception_msg = __import__ ('6-raise_exception').raise_exception_msg
+raise_exception_msg = __import__ ('6-raise_exception_msg').raise_exception_msg
 
 
 try:
--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    print(message)
+    print(message, end="")
     raise NameError
 
