5747c43bf55a07fcbde9b247ba5acca2f93bfdfa
brina-cloud <s.kaliza1@alustudent.com>
2025-10-31 03:11:26 -0700

    3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     result = 0
     try:
-       result = a / b
+        result = a / b
     except ZeroDivisionError:
-       result = None
+        result = None
     finally:
-       print("Inside result: {}".format(result))
+        print("Inside result: {}".format(result))
     return result
