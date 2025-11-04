f742d9c3a097dfe4008279d86d5dda94c6a28c7e
brina-cloud <s.kaliza1@alustudent.com>
2025-11-04 04:22:15 -0800

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 class Square:
-    """ An empty class """
-    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+""" An empty class """
+"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     pass
