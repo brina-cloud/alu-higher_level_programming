b4dafa65282214ae246e26a5a607c467c6ce9833
brina-cloud <s.kaliza1@alustudent.com>
2025-11-04 06:00:55 -0800

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class Square:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    def __init__(self, size):
+        self.size = size
