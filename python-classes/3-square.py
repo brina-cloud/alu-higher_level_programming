5925e3fd542965e9593eefee3ebd9d67f38a1fb1
brina-cloud <s.kaliza1@alustudent.com>
2025-11-05 03:08:47 -0800

    3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,8 +8,7 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValuError("size must br >= 0")
+            raise ValueError("size must  >= 0")
         self.__size = size
     def area(self):
         return self.__size ** 2
-
