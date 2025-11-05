849cf14787e146271785cb90fd8f49bb1624d1c8
brina-cloud <s.kaliza1@alustudent.com>
2025-11-05 14:01:28 -0800

    6

---
+++ python-classes/6-square.py
@@ -0,0 +1,49 @@
+#1/usr/bin/python3]
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class Square:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+         return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (
+             not isinstance(value, tuple)
+             or len(value) != 2
+             or not all(isinstance(num, int) for num in value)
+             or not all(num >= 0 for num in value)
+           ):
+            raise TypeError("position must be a tuple")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
