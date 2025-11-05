6d9d542778b1ee586d9d207e05ea391ca8fd50cb
brina-cloud <s.kaliza1@alustudent.com>
2025-11-05 14:14:39 -0800

    6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -42,7 +42,8 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for _ in range(self.__position[1]):
-            print()
-        for _ in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
