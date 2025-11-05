52444bcd8a0277b436c0d9edfe22ca0f8c36274d
brina-cloud <s.kaliza1@alustudent.com>
2025-11-05 04:28:17 -0800

    3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
