e25ad2e215e5229d017a8f3edd1dd3b485e05e39
brina-cloud <s.kaliza1@alustudent.com>
2025-11-06 04:02:47 -0800

    1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,7 +12,6 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-
     @width.setter
     def width(self, value):
         try:
@@ -28,7 +27,6 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
-
     @height.setter
     def height(self, value):
         try:
