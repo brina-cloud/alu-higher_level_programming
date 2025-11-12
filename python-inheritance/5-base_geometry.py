b39a0079540c0d079ab26c8b26132e5463581df2
brina-cloud <s.kaliza1@alustudent.com>
2025-11-12 03:36:33 -0800

    5-base_geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class BaseGeometry:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+
+    pass
