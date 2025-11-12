613d4f4a11f12d6d13506898f3bfc5c20999830e
brina-cloud <s.kaliza1@alustudent.com>
2025-11-12 05:13:19 -0800

    10-square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+
+    def __init__(self, size):
+       super().__init__(size, size)
+       self.integer_validator("size", size)
+       self.__size = size
+    def area(self):
+       """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+       return self.__size ** 2
