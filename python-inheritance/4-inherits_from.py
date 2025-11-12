ceb15c60884e3db684eb85bdef538f0abb2cc2f7
brina-cloud <s.kaliza1@alustudent.com>
2025-11-12 03:30:45 -0800

    4-inherits_from

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+def inherits_from(obj, a_class):
+    """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+    return type(obj) != a_class and issubclass(type(obj), a_class)
