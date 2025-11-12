83840f766a6d5dc2860003fe64da7c05a0f43606
brina-cloud <s.kaliza1@alustudent.com>
2025-11-12 05:26:28 -0800

    11-square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        return f"[Square] {self.__size}/{self.__size}"
