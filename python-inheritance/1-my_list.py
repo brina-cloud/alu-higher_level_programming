82c5616e1bbac5c4df21626439038b48368a8230
brina-cloud <s.kaliza1@alustudent.com>
2025-11-12 02:50:12 -0800

    testt file

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,5 +5,5 @@
 class MyList(list):
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     def print_sorted(self):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'""
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
         print(sorted(self))
