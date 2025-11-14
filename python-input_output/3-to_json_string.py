23bc1da41cd7f23a7c649920b41a3cbbc0e87079
brina-cloud <s.kaliza1@alustudent.com>
2025-11-14 10:16:04 -0800

    3-to_json_string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+"""Module that provides a function to convert objects to json strings"""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of an object as a string"""
+
+    return json.dump(my_obj)
