457b73bf01fc544386d874d9c4c750abe838f50d
brina-cloud <s.kaliza1@alustudent.com>
2025-11-14 11:57:41 -0800

    10-student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
-"""Module that defines a Student class"""
+"""Creating a class Student basing on file 9-student.py"""
 
 
 class Student:
-    """A class that defines a student"""
+    """The classes"""
+    first_name = None
+    last_name = None
+    age = None
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student with its attributes."""
+        """Initialising the attributes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieve a dictionary representation of a Student instance."""
-        if isinstance(attrs, list) and all(isinstance(attr, str):
-            for attr in attrs:
-                return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
-        return self.__dict__
+        """Returning a dictionary rep of the data"""
+        context = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+        if attrs is None or type(attrs) != list:
+            return context
+        else:
+            cont = {}
+            for item in attrs:
+                if type(item) != str:
+                    return context
+                if item in context.keys():
+                    cont[item] = context[item]
+            return cont
