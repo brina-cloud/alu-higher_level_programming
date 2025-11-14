37c5a0eef11277af83cde00975ae1f4665411aec
brina-cloud <s.kaliza1@alustudent.com>
2025-11-14 10:26:42 -0800

    4-from_json

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that provides a function to convert JSON strings to Python objects."""
+import json
+
+
+def from_json_string(my_str):
+    """Return a Python object represented by a JSON string."""
+    return json.loads(my_str)
