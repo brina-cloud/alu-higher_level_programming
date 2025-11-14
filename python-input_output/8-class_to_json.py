c96c565ff193db8920b84a640f5c66a9366379ef
brina-cloud <s.kaliza1@alustudent.com>
2025-11-14 11:17:27 -0800

    8-class_to_json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that provides a function to convert class instances to JSON-serializable dictionaries."""
+
+
+def class_to_json(obj):
+    """Return the dictionary description of an object for JSON serialization."""
+    return obj.__dict__
