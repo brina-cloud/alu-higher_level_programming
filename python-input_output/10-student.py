28931e1617c9cbbe6f8521bb8835e088f0352682
brina-cloud <s.kaliza1@alustudent.com>
2025-11-14 11:55:35 -0800

    10-student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,13 +6,14 @@ class Student:
     """A class that defines a student"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student with first_name, last_name, and age."""
+        """Initialize a Student with its attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """Retrieve a dictionary representation of a Student instance."""
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        if isinstance(attrs, list) and all(isinstance(attr, str):
+            for attr in attrs:
+                return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
         return self.__dict__
